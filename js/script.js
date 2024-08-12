eed89d19e0bd04788e90efb55bc0b9452e1d2a88
YUNA9627 <yuna2175@naver.com>
2024-08-12 20:46:09 +0900

    script 수정

--- js/script.js
+++ js/script.js
@@ -1,12 +1,33 @@
-let nav = document.querySelector('.top_nav > div');
+let nav = document.querySelector('.top_nav div');
+let gnb = nav.querySelector('nav');
+let logo = nav.querySelector('#logo');
+let largeLogo = 'img/Header_Top_LOGO_white.svg';
+let smallLogo = 'img/LOGO_red.svg';
 
 window.addEventListener('scroll',()=>{
   if(window.scrollY > 700){
     if(!nav.classList.contains('shrink')){
       nav.classList.add('shrink');
+      changeLogo(smallLogo);
+    }
+    if(!gnb.classList.contains('shrink')){
+      gnb.classList.add('shrink');
+    }
+    if(!logo.classList.contains('shrink')){
+      logo.classList.add('shrink');
     }
     }else{
       nav.classList.remove('shrink');
-  };
+      gnb.classList.remove('shrink');
+      logo.classList.remove('shrink');
+      changeLogo(largeLogo);
+  }
 });
-console.log(window.scrollY);
+
+function changeLogo(url){
+  logo.classList.add('hide');
+  setTimeout(()=>{
+    logo.setAttribute('src', url);
+    logo.classList.remove('hide');
+  }, 0);
+}
