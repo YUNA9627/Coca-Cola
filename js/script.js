f2b8df9114c62603e189c0bc6328917b1dc16014
YUNA9627 <yuna2175@naver.com>
2024-08-12 20:13:20 +0900

    script 수정

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 let nav = document.querySelector('.top_nav > div');
 
 window.addEventListener('scroll',()=>{
-  if(window.scrollY > 600){
+  if(window.scrollY > 700){
     if(!nav.classList.contains('shrink')){
       nav.classList.add('shrink');
+    }
     }else{
       nav.classList.remove('shrink');
-    }
-  }
-})
+  };
+});
 console.log(window.scrollY);
